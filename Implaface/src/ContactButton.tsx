25e989e3d562d91d1b4d5f1f5d96c9f958bb10cc
Sanmylly <sanmyllyw@gmail.com>
2025-05-06 08:49:09 -0300

    att

--- Implaface/src/ContactButton.tsx
+++ Implaface/src/ContactButton.tsx
@@ -2,10 +2,12 @@ import './styles/components/Buttons.css';
 
 const ContactButton = () => {
   return (
-
-      <button className="button">
+      <a href="https://api.whatsapp.com/send?phone=5581995424847&text=Olá,%20gostaria%20de%20agendar%20uma%20consulta" target='_blank' rel="noopener noreferrer">
+        <button className="button">
         Entre em contato
       </button>
+      </a>
+      
   );
 }
 
