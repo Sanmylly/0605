19a00b6dea3e7afdb0a667e1fc8e05478af89509
Sanmylly <sanmyllyw@gmail.com>
2025-05-06 09:27:08 -0300

    att

--- Implaface/vite.config.ts
+++ Implaface/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import { copyFileSync } from 'fs'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-static-json',
+      closeBundle() {
+        // Copia o arquivo `static.json` para `dist/` após o build
+        copyFileSync(resolve(__dirname, 'static.json'), resolve(__dirname, 'dist/static.json'))
+      }
+    }
+  ]
 })
